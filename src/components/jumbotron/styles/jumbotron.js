33f1ce982de48070f7f85737e0482fcbf98d4b9f
Guy Bramsworth <mpdev1987@gmail.com>
2022-02-28 05:21:37 -0800

    update

---
+++ src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,13 @@
+import styled from "styled-components/macro";
+
+export const Inner = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: ${({ direction }) => direction };
+    justify-content: space-between;
+    max-width: 1100px;
+    margin: auto;
+    width: 100%;
+`;
+
+export const Container = styled.div``
