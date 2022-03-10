89140a2422ba1071699e0d24b3834d7680330d61
Guy Bramsworth <mpdev1987@gmail.com>
2022-03-10 02:43:39 -0800

    update

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,12 +3,7 @@ import "firebase/compat/firestore"
 import "firebase/compat/auth" 
 
 const config = {
-    apiKey: "AIzaSyD2wcuO4nL-JGu4tVp8IOkNjFwG5yUepWY",
-    authDomain: "netflix-clone-3322.firebaseapp.com",
-    projectId: "netflix-clone-3322",
-    storageBucket: "netflix-clone-3322.appspot.com",
-    messagingSenderId: "1052838039041",
-    appId: "1:1052838039041:web:863f769dfc675ea4a3fa00",
+    apiKey: null
 }
 
 const firebase = Firebase.initializeApp(config)
